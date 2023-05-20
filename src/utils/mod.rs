a94d91212e3411fb95b8c7b65fa53a0e09c167bc
TheWisePigeon <zozozozeph@gmail.com>
2023-05-20 21:44:29 +0000

    remove error prone function

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,9 +1,3 @@
-use rand::distributions::Alphanumeric;
-use rand::Rng;
-
-pub fn generate_unique_string(length: usize) -> String {
-    let mut rng = rand::thread_rng();
-    let random_string: String = rng.sample_iter(&Alphanumeric).take(length).collect();
-    random_string
+pub fn generate_unique_string() -> String {
+    "".to_string()
 }
-
