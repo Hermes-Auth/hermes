b527d33d245d3177fc330e54ff0ad7240c8edc33
TheWisePigeon <zozozozeph@gmail.com>
2023-05-25 21:26:38 +0000

    feat: add request route

--- node/src/api/router.ts
+++ node/src/api/router.ts
@@ -5,5 +5,6 @@ import { auth_middleware } from "./auth_middlewares";
 const router = Router()
 
 router.route("/request").post( auth_middleware, request_code )
+router.route("/verify").post( auth_middleware, verify )
 
 export default router
