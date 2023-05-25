e50abe9cacda5a1ba19e412b67739818deda383e
TheWisePigeon <zozozozeph@gmail.com>
2023-05-25 21:26:54 +0000

    e-feat: verify user provided code

--- node/src/api/services.ts
+++ node/src/api/services.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from "express";
 import { z } from "zod"
-import bcrypt from "bcrypt"
 import sql from "../db";
 import { createTransport } from "nodemailer"
+import { markAsUntransferable } from "worker_threads";
+import { code_is_alive } from "../utils";
 const MAX = 999999
 const MIN = 100000
-const SALT_ROUNDS = 10
 
 export async function request_code(req: Request<{}, {}, { api_user: string }>, res: Response) {
     try {
@@ -24,7 +24,7 @@ export async function request_code(req: Request<{}, {}, { api_user: string }>, r
             const app = row[0]
             if (!app.active) return res.status(400).send({ message: "The specified app is not active" })
             const { SMTP_PASSWORD } = process.env
-            const code = Math.floor(Math.random() * (MAX - MIN + 1)) + MIN
+            const code = String(Math.floor(Math.random() * (MAX - MIN + 1)) + MIN) 
             const transporter = createTransport({
                 service: "gmail",
                 auth: {
@@ -43,11 +43,10 @@ export async function request_code(req: Request<{}, {}, { api_user: string }>, r
                     console.log(`Error while sending auth code: ${err}`)
                     return res.status(500).send()
                 }
-                const hash = bcrypt.hashSync(code.toString(), SALT_ROUNDS)
                 const generated_at = Date.now()
                 await sql`
-                insert into codes(code, ttl, generated_at, generated_for, app )
-                values(${hash}, ${data.ttl ?? app.codes_ttl}, ${generated_at}, ${data.target}, ${data.app_id})
+                    insert into codes(code, ttl, generated_at, generated_for, app )
+                    values(${code}, ${data.ttl ?? app.codes_ttl}, ${generated_at}, ${data.target}, ${data.app_id})
                 `
                 return res.status(200).send()
             })
@@ -59,10 +58,27 @@ export async function request_code(req: Request<{}, {}, { api_user: string }>, r
     }
 }
 
-export async function verify(req: Request, res: Response) {
+export async function verify(req: Request<{}, {}, { api_user:string }>, res: Response) {
     try {
         const schema = z.object({
-
+            code: z.string(),
+            user: z.string().email(),
+            app_id: z.string().uuid()
+        })
+        const validation_result = schema.safeParse(req.body)
+        const message = "Check your request body. Some fields might be missing or have incorrect types"
+        if(!validation_result.success) return res.status(400).send({ message })
+        const { data } = validation_result
+        await sql`select * from apps where id=${data.app_id} and owner=${req.body.api_user}`.then(async row=>{
+            if (row.length === 0) return res.status(400).send({ message: "No app found for the provided app id" })
+            const app = row[0]
+            if (!app.active) return res.status(400).send({ message: "The specified app is not active" })
+            await sql`select * from codes where code=${data.code} and app=${data.app_id} and generated_for=${data.user} and used=${false}`.then(async row=>{
+                if(row.length===0) return res.status(400).send()
+                const code = row[0]
+                code_is_alive(Number.parseInt(code.ttl), Number.parseInt(code.generated_at))
+            })
+            return res.status(200).send()
         })
     } catch (err) {
         console.error(`Error while verifying auth code ${err}`)
