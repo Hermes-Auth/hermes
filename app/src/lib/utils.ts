b21ce09200f6bdf5dc43dd767d16a5c72c421ceb
TheWisePigeon <zozozozeph@gmail.com>
2023-05-28 20:44:41 +0000

    fix: typo in code_alive function

---
+++ app/src/lib/utils.ts
@@ -0,0 +1,10 @@
+export function code_is_alive( code_ttl:number, generated_at:number ){
+    try {
+        const now = Date.now()
+        let diff_in_min = Math.floor( (now - generated_at)/60000 )%60
+        return code_ttl>diff_in_min
+    } catch (err) {
+        console.error(`Error while checking code lifespan: ${err}`)
+        throw Error("CODE_ALIVE_ERR")
+    }
+}
