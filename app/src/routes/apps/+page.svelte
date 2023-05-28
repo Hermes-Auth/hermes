20cb9915be678c13b6e09bf3177b9f439d1b4000
TheWisePigeon <zozozozeph@gmail.com>
2023-05-28 22:21:53 +0000

    add apps tab

---
+++ app/src/routes/apps/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		const token = localStorage.getItem('token') ?? '';
+		if (token === '') {
+			alert('You are not authenticated');
+			goto('/');
+		}
+	});
+</script>
