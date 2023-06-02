ba1015e39545acf12e5044a6de79857273703065
TheWisePigeon <zozozozeph@gmail.com>
2023-06-02 17:59:10 +0000

    minor updates

--- src-rs/lib.rs
+++ src-rs/lib.rs
@@ -1,7 +1,32 @@
-use rand::seq::SliceRandom;
+extern crate lettre;
 
-pub fn choose_starter() -> String {
-    let pokemons = vec!["Bulbasaur", "Charmander", "Squirtle", "Pikachu"];
-    let starter = pokemons.choose(&mut rand::thread_rng()).unwrap();
-    starter.to_string()
+use lettre::message::header::ContentType;
+use lettre::transport::smtp::authentication::Credentials;
+use lettre::{Message, SmtpTransport, Transport};
+
+pub fn send_mail(receiver: String, text: String, subject: String) -> bool {
+    let email = Message::builder()
+        .from("pigeondev01@gmail.com".parse().unwrap())
+        .to(receiver.parse().unwrap())
+        .subject(subject)
+        .header(ContentType::TEXT_HTML)
+        .body(text)
+        .unwrap();
+    let creds = Credentials::new(
+        "pigeondev01@gmail.com".to_owned(),
+        "fkyndapgxobbxodq".to_owned(),
+    );
+    let mailer = SmtpTransport::relay("smtp.gmail.com")
+        .unwrap()
+        .credentials(creds)
+        .build();
+    match mailer.send(&email) {
+        Ok(_)=>{
+            true
+        },
+        Err(err)=>{
+            println!("Error while sending mail {err}");
+            false
+        }
+    }
 }
