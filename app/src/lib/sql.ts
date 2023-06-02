49693897a031e06928a16190b395b95288b46356
TheWisePigeon <zozozozeph@gmail.com>
2023-06-02 14:53:06 +0000

    rusty beginnings

--- app/src/lib/sql.ts
+++
@@ -1,6 +0,0 @@
-import dotenv from "dotenv"
-dotenv.config()
-
-import { sql } from "@vercel/postgres";
-
-export default sql
--- app/src/lib/store/index.ts
+++
@@ -1,5 +0,0 @@
-import { writable } from "svelte/store"
-
-export const current_tab = writable("auth")
-
-export const user_is_logged_in = writable(false)
